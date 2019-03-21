bddc6f9db4751ca2e15c672e648b6b14d9a49d6f
NJubainville1 <njubainville@gmail.com>
2019-03-20 20:45:31 -0600

    Updated content

---
+++ js/menu/menu.js
@@ -0,0 +1,18 @@
+window.addEventListener("DOMContentLoaded", function (event) {
+    let isOpen = false;
+    const menu = document.querySelector('#mobile-sub-menu');
+    const menuOpenButton = document.querySelector("#mobile-menu-button");
+    /*
+    https://greensock.com/docs/Easing/Bounce
+    easing graph
+    */
+    //  code to animate mobile navigation menu.
+    menuOpenButton.onclick = (e) => {
+        isOpen = !isOpen;
+        if (!isOpen) {
+            TweenMax.to(menu, 0.8, { top: "-55vh", ease: Power4.easeOut });
+        } else {
+            TweenMax.to(menu, 0.5, { top: "48px", ease: Power4.easeOut });
+        }
+    }
+});
